872f45e7603647cf79ee5192e6c01fac8a85e90a
sjunkim95 <sangjunkim23@gmail.com>
2024-07-22 13:40:29 -0700

    7/22-8

--- part6.py
+++ part6.py
@@ -7,34 +7,38 @@ class PeekableIterator:
         return self
 
     def __next__(self):
-        # if self.counter >= len(self.iterable):
-        if self.counter >= self.iterable:
+        self.index = self.counter
+        if self.counter >= len(self.iterable):
+       # if self.counter >= self.iterable:
             raise StopIteration
         else:
             self.counter += 1
-            return self.counter
-        #  return self.iterable[self.counter]
+           # return self.counter
+            return self.iterable[self.index]
 
     def peek(self):
         #return self.iterable[self.counter+1]
-        if self.counter < self.iterable:
-            return self.counter
+        if self.counter >= len(self.iterable):
+           # return self.counter
+           return self.iterable[self.counter + 1]
         else:
             raise StopIteration("Index is out of range")
 
     def has_next(self):
         #if self.counter >= len(self.iterable):
-        if self.counter >= self.iterable:
+        if self.counter >= len(self.iterable):
             return False
         else:
             return True
 
 if __name__ == '__main__':
-     run = PeekableIterator(10)
-     print("peek는", run.peek())
+     run = PeekableIterator([1,2,3,4,5,6])
+
      for i in run:
          print(i)
-     print(run.has_next())
+         print(run.has_next())
+
+     #print("peek는", run.peek())
 
 
 
