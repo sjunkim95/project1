c7313ee802c386895feb72aa7a496ed22ecc18bf
sjunkim95 <sangjunkim23@gmail.com>
2024-07-22 02:13:38 -0700

    7/22-4

--- part6.py
+++ part6.py
@@ -7,24 +7,30 @@ class PeekableIterator:
         return self
 
     def __next__(self):
-        if self.counter >= len(self.iterable):
+       # if self.counter >= len(self.iterable):
+        if self.counter >= self.iterable:
             raise StopIteration
-    #    elif self.counter >= self.iterable:
-   #         raise StopIteration
+        """
+            elif self.counter >= self.iterable:
+            raise StopIteration
+        """
         else:
             self.counter += 1
-            return self.iterable[self.counter]
+        #  return self.iterable[self.counter]
+            return self.counter
 
     def peek(self):
-        return self.iterable[self.counter+1]
+        #return self.iterable[self.counter+1]
+        return self.counter
 
     def has_next(self):
-        if self.counter >= len(self.iterable):
+        #if self.counter >= len(self.iterable):
+        if self.counter >= self.iterable:
             return False
         else:
             return True
 
 
 #if __name__ == '__main__':
-   # run = PeekableIterator([1, 2, 3, 4, 5, 6, 7])
-    #print(run.has_next())
+# run = PeekableIterator([1, 2, 3, 4, 5, 6, 7])
+#print(run.has_next())
